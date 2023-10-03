e3a61e756142f00bc58d8e0496e9b3b85664ec1c
Aravix <49846328+aravikusu@users.noreply.github.com>
2023-10-04 01:11:07 +0200

    Delete build directory
    Was never meant to be here.

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/ultracockdrive/repos/aravoxel-cpp
-# Build directory: /mnt/ultracockdrive/repos/aravoxel-cpp/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
